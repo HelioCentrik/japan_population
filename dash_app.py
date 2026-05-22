e0ed9a0bb48531ffa96a8022790331c31fb3eefa
HelioCentrik <60416621+HelioCentrik@users.noreply.github.com>
2026-05-22 16:01:36 -0400

    fix: prevent title from changing

--- dash_app.py
+++ dash_app.py
@@ -7,7 +7,7 @@ from flask import send_from_directory
 from app.aesthetics.index_string import INDEX_STRING
 from app.aesthetics.plotly_template import register_plotly_template
 
-app = dash.Dash(__name__, title="少子高齢化 A Century of Japan's Population")
+app = dash.Dash(__name__, title="少子高齢化 A Century of Japan's Population", update_title=None)
 app.index_string = INDEX_STRING
 register_plotly_template()
 server = app.server
